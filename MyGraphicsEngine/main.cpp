5775cf3fc4c376df8ce72503b04861ada6aed907
ozu <oscar.s@digipen.edu>
2022-08-04 18:01:19 +0200

    Added a process input function

--- MyGraphicsEngine/main.cpp
+++ MyGraphicsEngine/main.cpp
@@ -24,6 +24,9 @@ const char* fragmentShaderSource = "#version 330 core\n"
 "}\n\0";
 
 
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void processInput(GLFWwindow* window);
+
 
 int main()
 {
@@ -139,6 +142,9 @@ int main()
 	// Main while loop
 	while (!glfwWindowShouldClose(window))
 	{
+		//Process Input from the user 
+		processInput(window);
+
 		// Specify the color of the background
 		glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
 		// Clean the back buffer and assign the new color to it
@@ -202,4 +208,20 @@ int main()
 	// Terminate GLFW before ending the program
 	glfwTerminate();
 	return 0;
+}
+// process all input: query GLFW whether relevant keys are pressed/released this frame and react accordingly
+// ---------------------------------------------------------------------------------------------------------
+void processInput(GLFWwindow* window)
+{
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, true);
+}
+
+// glfw: whenever the window size changed (by OS or user resize) this callback function executes
+// ---------------------------------------------------------------------------------------------
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+	// make sure the viewport matches the new window dimensions; note that width and 
+	// height will be significantly larger than specified on retina displays.
+	glViewport(0, 0, width, height);
 }
